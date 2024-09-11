ed7a6732a28bb48485638b1f73fa1c6e4a02ddd8
H7CDR <110644782+H7CDR@users.noreply.github.com>
2024-09-11 16:56:10 +1000

    SQL generated
    SQL generated

---
+++ El_Gran_PetCare/dbo.OwnerTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OwnerTable]
+(
+	[ownerID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ownerName] NVARCHAR(50) NULL, 
+    [ownerPhone] NVARCHAR(50) NULL, 
+    [ownerEmail] NVARCHAR(50) NULL
+)
