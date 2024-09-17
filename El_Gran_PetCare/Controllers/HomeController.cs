3154d1dfd1abaddc4cadc360c6d1c89ebb7511c5
H7CDR <110644782+H7CDR@users.noreply.github.com>
2024-09-17 22:07:57 +1000

    Codes Done

--- El_Gran_PetCare/Controllers/HomeController.cs
+++ El_Gran_PetCare/Controllers/HomeController.cs
@@ -1,30 +1,85 @@
-﻿using System;
+﻿using El_Gran_PetCare.Models;
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace El_Gran_PetCare.Controllers
 {
     public class HomeController : Controller
     {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PetCare_DB;Integrated Security=True;";
+
         public ActionResult Index()
         {
+            var appointments = new List<AppointmentModels>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Appointment";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                  
+                    appointments.Add(new AppointmentModels
+                    {
+                        appointmentID = reader.GetInt32(0),
+                        ownerID = reader.GetInt32(1),
+                        petID = reader.GetInt32(2),
+                        vetID = reader.GetInt32(3),
+                        appointmentDate = reader.GetDateTime(4),
+                        appointmentReason = reader.GetString(5),
+                        appointmentNote = reader.GetString(6),
+                    });
+                    
+                }
+            }
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View(new AppointmentModels());
         }
 
-        public ActionResult Contact()
+        //Post: Appointment Create also Pet and Owner
+        [HttpPost]
+        public ActionResult Create(AppointmentModels appointmentModel, OwnerClass ownerClass, PetClass petClass, VetClass vetClass)
         {
-            ViewBag.Message = "Your contact page.";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string ownerQuery = "INSERT INTO OwnerTable VALUES (@ownerName, @ownerPhone, @ownerEmail)";
+                string petQuery = "INSERT INTO petTable VALUES(@petName, @petSpecies, @petBreed, @petGender, @petAge, @ownerID";
+                string appointmentQuery = "INSERT INTO AppointmentTable VALUES (@owenerID, @petID, @vetID, @appointmentDate, @appointmentReason, @appointNote";
+                SqlCommand ownerCmd = new SqlCommand(ownerQuery, connection);
+                ownerCmd.Parameters.AddWithValue("@ownerName", ownerClass.ownerName);
+                ownerCmd.Parameters.AddWithValue("@ownerPhone", ownerClass.ownerPhone);
+                ownerCmd.Parameters.AddWithValue("@ownerEmail", ownerClass.ownerEmail);
+                ownerCmd.ExecuteNonQuery();
+                SqlCommand petCmd = new SqlCommand(petQuery, connection);
+                petCmd.Parameters.AddWithValue("@petName", petClass.petName);
+                petCmd.Parameters.AddWithValue("@petSpecies", petClass.petSpecies);
+                petCmd.Parameters.AddWithValue("@petBreed", petClass.petBreed);
+                petCmd.Parameters.AddWithValue("@petGender", petClass.petGender);
+                petCmd.Parameters.AddWithValue("@petAge", petClass.petAge);
+                petCmd.Parameters.AddWithValue("@ownerID", ownerClass.ownerID);
+                petCmd.ExecuteNonQuery();
+                SqlCommand appointmentCmd = new SqlCommand(appointmentQuery, connection);
+                appointmentCmd.Parameters.AddWithValue("@ownerID", ownerClass.ownerID);
+                appointmentCmd.Parameters.AddWithValue("@petID", petClass.petID);
+                appointmentCmd.Parameters.AddWithValue("@vetID", vetClass.vetID);
+                appointmentCmd.Parameters.AddWithValue("@appointmentDate", appointmentModel.appointmentDate);
+                appointmentCmd.Parameters.AddWithValue("@appointmentReason", appointmentModel.appointmentReason);
+                appointmentCmd.Parameters.AddWithValue("@appointmentNote", appointmentModel.appointmentNote);
+                appointmentCmd.ExecuteNonQuery();
 
-            return View();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ El_Gran_PetCare/Models/VetClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace El_Gran_PetCare.Models
+{
+}
