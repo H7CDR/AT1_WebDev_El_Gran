28b699fbbca33ff02b36e38c6fb00b9deb3fe0bb
H7CDR <110644782+H7CDR@users.noreply.github.com>
2024-11-08 14:56:26 +1100

    Commit
    asdasd

---
+++ El_Gran_PetCare/dbo.PetTable_1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PetTable] (
+    [petID]      INT           IDENTITY (1, 1) NOT NULL,
+    [petName]    NVARCHAR (50) NULL,
+    [petSpecies] NVARCHAR (50) NULL,
+    [petBreed]   NVARCHAR (50) NULL,
+    [petGender]  NVARCHAR(50)           NULL,
+    [petAge]     INT           NULL,
+    [ownerID]    INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([petID] ASC),
+    CONSTRAINT [FK_PetTable_ownerID] FOREIGN KEY ([ownerID]) REFERENCES [dbo].[OwnerTable] ([ownerID])
+);
+
